bf1882383ccb2c9d17f774d446ba2476450cb5a9
Mamon Aburawi <mamon.aburawi@gmail.com>
2022-10-02 13:08:37 +0200

    update novel details design

--- app/src/main/java/com/shaimaziyad/khayal/screens/novelDetails/NovelDetails.kt
+++ app/src/main/java/com/shaimaziyad/khayal/screens/novelDetails/NovelDetails.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.shaimaziyad.khayal.R
 import com.shaimaziyad.khayal.data.NovelData
 import com.shaimaziyad.khayal.databinding.NovelDetailsBinding
+
 import com.shaimaziyad.khayal.utils.Constants
 import com.shaimaziyad.khayal.utils.showMessage
 
@@ -18,15 +19,14 @@ class NovelDetails : Fragment() {
 
     private lateinit var binding: NovelDetailsBinding
     private var novel: NovelData? = null
+    private val chapterController by lazy { ChapterController() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
-        // Inflate the layout for this fragment
         binding = NovelDetailsBinding.inflate(layoutInflater)
 
         setData()
         setViews()
-
 
 
         return binding.root
@@ -37,19 +37,32 @@ class NovelDetails : Fragment() {
         catch (ex: Exception) { null }
     }
 
+    private fun setChapterAdapter(){
+
+        // init data
+        chapterController.setData(novel?.pdfs)
+
+        /** on chapter click **/
+        chapterController.clickListener = object: ChapterController.OnClickListener {
+            override fun onChapterClicked(pdfUri: String) {
+                val data = bundleOf(Constants.PDF_KEY to pdfUri)
+                findNavController().navigate(R.id.action_novelDetails_to_viewPdf,data)
+
+            }
+        }
+        binding.rvChapters.adapter = chapterController.adapter
+    }
+
+
     private fun setViews() {
         binding.apply {
 
             novelData = novel
             lifecycleOwner = this@NovelDetails
 
-
+            setChapterAdapter()
             setAppBar()
 
-            /** button read **/
-            btnRead.setOnClickListener {
-                findNavController().navigate(R.id.action_novelDetails_to_novelPdf)
-            }
 
         }
     }
@@ -69,11 +82,13 @@ class NovelDetails : Fragment() {
 
             /** button options **/
             btnOptions.setOnClickListener {
+                // TODO: add some options
                 showMessage("options")
             }
 
             /** button share **/
             btnShare.setOnClickListener {
+                // todo add share feature
                 showMessage("share")
             }
 
