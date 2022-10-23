e8cceb2b52e0b45c44e82a70e80ae56bec2ce2f0
Mamon Aburawi <mamon.aburawi@gmail.com>
2022-10-23 12:46:58 +0200

    add home option sheet and implement complex data to home screen

--- app/src/main/java/com/shaimaziyad/khayal/screens/home/Home.kt
+++ app/src/main/java/com/shaimaziyad/khayal/screens/home/Home.kt
@@ -1,24 +1,24 @@
 package com.shaimaziyad.khayal.screens.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.widget.PopupMenu
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.shaimaziyad.khayal.R
-import com.shaimaziyad.khayal.data.NovelData
+import com.shaimaziyad.khayal.data.Novel
 import com.shaimaziyad.khayal.databinding.HomeBinding
 import com.shaimaziyad.khayal.screens.notifications.NotificationsViewModel
 import com.shaimaziyad.khayal.screens.profile.ProfileViewModel
-import com.shaimaziyad.khayal.sheets.FilterNovelSheet
+import com.shaimaziyad.khayal.sheets.HomeOptionSheet
 import com.shaimaziyad.khayal.utils.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
+@SuppressLint("NotifyDataSetChanged")
 class Home: Fragment() {
 
     companion object {
@@ -33,20 +33,25 @@ class Home: Fragment() {
 
     private val notifyViewModel by sharedViewModel<NotificationsViewModel>()
 
+    private lateinit var homeAdapter: HomeAdapter
+
     private val novelAdapter by lazy { NovelAdapter() }
-    private lateinit var filterNovel: FilterNovelSheet
+
+    private lateinit var optionSheet: HomeOptionSheet
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         binding = HomeBinding.inflate(layoutInflater)
-        filterNovel = FilterNovelSheet(requireContext(),binding.novelSheet,this)
-
+        optionSheet = HomeOptionSheet(binding.optionsSheet,this) // home option sheet
+        homeAdapter = HomeAdapter(this)
 
         // todo: add share pref for user between screens
 
         setViews()
         setObserves()
 
+
+        // todo: you can call the home view model in splash screen to reduce the time of loading data
 
 
         return binding.root
@@ -55,17 +60,9 @@ class Home: Fragment() {
 
     override fun onResume() {
         super.onResume()
-
-        val isFirstLoad = viewModel.novels.value.isNullOrEmpty()
-        if (!isFirstLoad) { // show progress only for first time load
-            
-        }
-
-        viewModel.loadNovels()
-        profileViewModel.loadUser()
+//        viewModel.loadNovels()
+//        profileViewModel.loadUser()
         notifyViewModel.loadNotificationsByUserId()
-
-
 
     }
 
@@ -77,33 +74,49 @@ class Home: Fragment() {
             profileModel = profileViewModel
             lifecycleOwner = this@Home
 
-            setAdapter()
+//            setAdapter()
+//
+            setMixedAdapter()
+
             setToolBar()
 
-            filterNovelStatus()
+//            filterNovelStatus()
 
             /** button add novel **/
             btnAddNovel.setOnClickListener {
                 navigateToAddEditNovel(isEdit = false, novel = null) // if isEdit false that means the user will add new novel else the user will edit old novel
             }
 
-            /** swipe to refresh novels **/
-            refreshNovels.setOnRefreshListener {
-                viewModel.loadNovels()
-                profileViewModel.loadUser()
-                notifyViewModel.loadNotificationsByUserId()
-            }
         }
     }
+
+    private fun setMixedAdapter() {
+        binding.novelsRv.apply {
+            itemAnimator = DefaultItemAnimator()
+            adapter = homeAdapter
+        }
+    }
+
 
 
     private fun setObserves() {
         viewModel.apply {
 
-            /** novels live data **/
-            novels.observe(viewLifecycleOwner){ novels->
-                if (!novels.isNullOrEmpty()){ // novel is exist.
-                    novelAdapter.submitList(novels)
+
+//            /** novels live data **/
+//            novels.observe(viewLifecycleOwner){ novels->
+//                if (!novels.isNullOrEmpty()){ // novel is exist.
+////                    novelAdapter.submitList(novels)
+//
+//
+//                }
+//            }
+
+            /** mixed live data **/
+            mixedData.observe(viewLifecycleOwner){ data ->
+                if (!data.isNullOrEmpty()) {
+                    homeAdapter.items = data
+                    homeAdapter.notifyDataSetChanged()
                 }
             }
 
@@ -111,148 +124,176 @@ class Home: Fragment() {
             /** unRead live data **/
             notifyViewModel.unRead.observe(viewLifecycleOwner){ unReads ->
                 if (!unReads.isNullOrEmpty()){
-                    binding.notify.badgeNumber.show()
-                    binding.notify.badgeNumber.text = unReads.size.toString()
+
+                    binding.searchLayout.notify.badgeNumber.show()
+                    binding.searchLayout.notify.badgeNumber.text = unReads.size.toString()
                 }else{
-                    binding.notify.badgeNumber.hide()
+                    binding.searchLayout.notify.badgeNumber.hide()
                 }
             }
+
 
 
         }
     }
 
-    private fun filterNovelStatus(){
-        filterNovel.filterStatus = object: FilterNovelSheet.FilterStatus {
+//    private fun filterNovelStatus(){
+//        filterNovel.filterStatus = object: FilterNovelSheet.FilterStatus {
+//
+//            override fun onFilter(filtered: List<Novel>) {
+//                novelAdapter.submitList(filtered)
+//            }
+//
+//            override fun clearFilter() {
+//               novelAdapter.submitList(viewModel.novels.value ?: emptyList())
+//            }
+//
+//            override fun onSheetOpen() {
+//                binding.btnAddNovel.hide()
+//            }
+//
+//            override fun onSheetClose() {
+//                if (profileViewModel.user.value?.userType == UserType.ADMIN.name){
+//                    binding.btnAddNovel.show()
+//                }
+//
+//            }
+//
+//        }
+//    }
 
-            override fun onFilter(filtered: List<NovelData>) {
-                novelAdapter.submitList(filtered)
-            }
 
-            override fun clearFilter() {
-               novelAdapter.submitList(viewModel.novels.value ?: emptyList())
-            }
-
-            override fun onSheetOpen() {
-                binding.btnAddNovel.hide()
-            }
-
-            override fun onSheetClose() {
-                if (profileViewModel.user.value?.userType == UserType.ADMIN.name){
-                    binding.btnAddNovel.show()
-                }
-
-            }
-
-        }
-    }
 
     private fun setToolBar() {
-        binding.apply {
+        binding.searchLayout.apply {
 
+            user = profileViewModel.user.value!!
 
             /** button profile **/
             btnProfile.setOnClickListener {
-                navigateToProfile()
+                optionSheet.notifyCounts = notifyViewModel.unRead.value!!.size
+                optionSheet.user = profileViewModel.user.value!!
+                optionSheet.showSheet()
+//                navigateToProfile()
             }
 
-            /** button options **/
-            btnOptions.setOnClickListener {
-                setHomeOption()
-            }
+
+//            /** button options **/
+//            btnOptions.setOnClickListener {
+//                setHomeOption()
+//            }
 
 
             /** button notification **/
-            notify.btnNotify.setOnClickListener {
+            notify.root.setOnClickListener {
                 navigateToNotification()
             }
 
-
-
-
-
             /** button search **/
-            search.setOnEditorActionListener { textView, actionId, _ ->
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    val query = textView.text.trim().toString()
-                    novelAdapter.submitList(viewModel.searchByNovelTitle(query))
-                    hideKeyboard()
-                    true
-                } else { false }
+            btnSearch.setOnClickListener {
+                navigateToSearch(null)
             }
+
+
+//            /** button search **/
+//            btnSearch.setOnEditorActionListener { textView, actionId, _ ->
+//                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+//                    val query = textView.text.trim().toString()
+//                    novelAdapter.submitList(viewModel.searchByNovelTitle(query))
+//                    hideKeyboard()
+//                    true
+//                } else { false }
+//            }
 
 
         }
     }
 
 
+    fun navigateToSearch(category: List<Novel>?) {
+        val data = bundleOf(Constants.CATEGORY_KEY to category)
+        findNavController().navigate(R.id.action_home_to_search,data)
+    }
 
-    private fun navigateToProfile() {
+    fun navigateToProfile() {
         findNavController().navigate(R.id.action_home_to_profile)
     }
 
 
-    private fun navigateToUsers() {
+    fun navigateToUsers() {
         findNavController().navigate(R.id.action_home_to_users)
     }
 
 
-    private fun navigateToNotification() {
+    fun navigateToNotification() {
         findNavController().navigate(R.id.action_home_to_notifications)
     }
 
-    private fun setHomeOption() {
-        val popupMenu = PopupMenu(requireContext(),binding.btnOptions)
-        popupMenu.menuInflater.inflate(R.menu.home_menu,popupMenu.menu)
 
-        if (!viewModel.isCustomer.value!!) { // if admin
-            popupMenu.menu.add(Menu.NONE, ITEM_USERS,Menu.NONE,R.string.users) // add item to menu
+//    private fun setHomeOption() {
+//        val popupMenu = PopupMenu(requireContext(),binding.btnOptions)
+//        popupMenu.menuInflater.inflate(R.menu.home_menu,popupMenu.menu)
+//
+//        if (!viewModel.isCustomer.value!!) { // if admin
+//            popupMenu.menu.add(Menu.NONE, ITEM_USERS,Menu.NONE,R.string.users) // add item to menu
+//
+//        }
+//
+//        popupMenu.setOnMenuItemClickListener { item ->
+//            when (item.itemId) {
+//                R.id.item_filter -> {
+//                    filterNovel.novels = viewModel.novels.value ?: emptyList()
+//                    filterNovel.showSheet()
+//                }
+//                ITEM_USERS -> { navigateToUsers() }
+//            }
+//            true
+//        }
+//        popupMenu.show()
+//    }
+
+
+
+    fun navigateToAddEditNovel(isEdit: Boolean, novel: Novel?) {
+
+        val userType = profileViewModel.user.value?.userType
+
+        if (UserType.USER.name == userType) { // user
+            val data = bundleOf(Constants.NOVEL_KEY to novel)
+            findNavController().navigate(R.id.action_userHome_to_novelDetails,data)
 
         }
-
-        popupMenu.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.item_filter -> {
-                    filterNovel.novels = viewModel.novels.value ?: emptyList()
-                    filterNovel.showSheet()
-                }
-                ITEM_USERS -> { navigateToUsers() }
-            }
-            true
+        if (userType == UserType.ADMIN.name){
+            val data = bundleOf(Constants.IS_EDIT_KEY to isEdit, Constants.NOVEL_KEY to novel)
+            findNavController().navigate(R.id.action_home_to_addEditNovel,data)
         }
-        popupMenu.show()
+
     }
 
 
-    private fun navigateToAddEditNovel(isEdit: Boolean, novel: NovelData?) {
-        val data = bundleOf(Constants.IS_EDIT_KEY to isEdit, Constants.NOVEL_KEY to novel)
-        findNavController().navigate(R.id.action_home_to_addEditNovel,data)
-    }
-
-
-    private fun setAdapter(){
-
-        /** onNovelClick listener **/
-        novelAdapter.clickListener = object: NovelAdapter.NovelClickListener {
-
-            override fun onClick(novel: NovelData, index: Int) {
-                val userType = profileViewModel.user.value?.userType
-
-                if (UserType.USER.name == userType) { // user
-                    val data = bundleOf(Constants.NOVEL_KEY to novel)
-                    findNavController().navigate(R.id.action_userHome_to_novelDetails,data)
-
-                }
-                if (userType == UserType.ADMIN.name){
-                    navigateToAddEditNovel(true,novel)
-                }
-
-            }
-        }
-
-        binding.novelsRv.adapter = novelAdapter
-    }
-
+//    private fun setAdapter(){
+//
+//        /** onNovelClick listener **/
+//        novelAdapter.clickListener = object: NovelAdapter.ClickListener {
+//
+//            override fun onClick(novel: Novel, index: Int) {
+//                val userType = profileViewModel.user.value?.userType
+//
+//                if (UserType.USER.name == userType) { // user
+//                    val data = bundleOf(Constants.NOVEL_KEY to novel)
+//                    findNavController().navigate(R.id.action_userHome_to_novelDetails,data)
+//
+//                }
+//                if (userType == UserType.ADMIN.name){
+//                    navigateToAddEditNovel(true,novel)
+//                }
+//
+//            }
+//        }
+//
+//        binding.novelsRv.adapter = novelAdapter
+//    }
+//
 
 
 
